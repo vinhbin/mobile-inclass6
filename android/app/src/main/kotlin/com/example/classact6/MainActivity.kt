a12acbb435e7ce2ee6502bbe8fa18de27b467b5b
vinhbin <binepzai2004@gmail.com>
2025-02-24 14:23:05 -0500

    slider works, age increase and decrese elevated button works

---
+++ android/app/src/main/kotlin/com/example/classact6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.classact6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+
+void main() {
+  setupWindow();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Counter(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+const double windowWidth = 360;
+const double windowHeight = 640;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Counter');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(
+        Rect.fromCenter(
+          center: screen!.frame.center,
+          width: windowWidth,
+          height: windowHeight,
+        ),
+      );
+    });
+  }
+}
+
+class Counter with ChangeNotifier {
+  int _value = 0;
+
+  int get value => _value;
+
+  void increment() {
+    _value += 1;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _value -= 1;
+    notifyListeners();
+  }
+
+  void setValue(double newValue) {
+    _value = newValue.toInt();
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Age counter')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<Counter>(
+              builder:
+                  (context, counter, child) => Text(
+                    ' I am ${counter.value} years old',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+            ),
+            Consumer<Counter>(
+              builder:
+                  (context, counter, child) => Slider(
+                    min: 0,
+                    max: 100,
+                    value: counter.value.toDouble(),
+                    onChanged: (value) {
+                      counter.setValue(value);
+                    },
+                  ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                var counter = context.read<Counter>();
+                counter.increment();
+              },
+              child: Text('Increase Age'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                var counter = context.read<Counter>();
+                counter.decrement();
+              },
+              child: Text('Reduce Age'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          var counter = context.read<Counter>();
+          counter.increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
