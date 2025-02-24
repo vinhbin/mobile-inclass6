44baf7e8dd45953d4f56cc9ea35fd392db53c0f3
vinhbin <binepzai2004@gmail.com>
2025-02-24 16:36:34 -0500

    list for milestone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io' show Platform;
+import 'dart:math';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -63,7 +64,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFEFEFEF)),
       home: const MyHomePage(),
     );
   }
@@ -71,6 +72,23 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+  static List<String> milestones = [
+    "Baby",
+    "Child",
+    "Teenager",
+    "The Dark Ages",
+    "About time for Fatherhood",
+    "Unc",
+    "Retirement",
+    "How Old am I?",
+  ];
+  String findMilestones(int value) {
+    if (value < 20) {
+      return milestones[((7 + value) ~/ 10).clamp(0, 2)];
+    } else {
+      return milestones[min(milestones.length - 1, (value ~/ 10) + 1)];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,6 +98,11 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Consumer<Counter>(
+              builder:
+                  (context, counter, child) =>
+                      Text(findMilestones(counter.value)),
+            ),
             Consumer<Counter>(
               builder:
                   (context, counter, child) => Text(
